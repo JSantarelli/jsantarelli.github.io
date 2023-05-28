b2846e0f507d9c852adc9b06d68fa84f0ddd9be8
Julio Nicolas Santarelli <santarellijulio@gmail.com>
2023-05-28 15:09:35 -0300

    doc(juliosantarelli.com): Modifica CNAME doc

--- prescripcion-digital/assets/js/funciones.js
+++ prescripcion-digital/assets/js/funciones.js
@@ -1,43 +1,18 @@
-meds = ['ABIRATERONA', 'DOXILAMINA', 'IBUPROFENO', 'OMEPRAZOL', 'OXICODONA', 'SALBUTAMOL', 'VALSARTAN']
 
-let med = parseInt(prompt('Ingrese una cantidad de articulos (una cifra): '))
+let stock = parseInt(prompt('Ingrese una cantidad de articulos (una cifra): '))
 
+// Función
 function recorrerMedicamentos() {
-  for(let i = 0; i < meds; i++) {
-    console.log(meds, i)
+  // Ciclo
+  for(let i = 0; i < stock; i++) {
+    console.log(stock, i)
     }
-    if (med >= meds.length) {
+    // Condicional
+    if (stock >= 7) {
       console.error('No tenemos esa cantidad de articulos')
     } else {
-      console.warn('Estos son los articulos en stock: ', meds)
+      console.warn('Estos son los articulos en stock: ', stock)
       }
   }
 
 recorrerMedicamentos()
-
-
-// Obtengo cantidad de medicación inicial
-let cantidadMedicacion = parseInt(prompt('Ingrese cantidad de comprimidos disponibles: '))
-console.log(cantidadMedicacion)
-
-// Obtengo duración del tratamiento
-let duracionTratamiento = parseInt(prompt('Ingrese duración del tratamiento (días): '))
-console.log(duracionTratamiento)
-
-// Obtengo duración del tratamiento
-let dosisDiaria = parseInt(prompt('Cuántos comprimidos diarios debe consumir? '))
-console.log(dosisDiaria)
-
-calcularDosis = () => {
-  let dosisTotal = dosisDiaria * duracionTratamiento
-  let diferenciaDosis = dosisTotal - cantidadMedicacion
-
-  if (dosisTotal <= cantidadMedicacion) {
-    alert('Tienes suficiente medicación: ' + dosisTotal + ' comprimidos')
-  } else {
-    alert('Necesitarás: ' + diferenciaDosis + ' comprimidos extra')
-    console.warn('Necesitarás: ' + diferenciaDosis + ' comprimidos extra')
-  }
-}
-
-calcularDosis()
