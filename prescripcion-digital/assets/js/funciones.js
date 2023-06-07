3fa600865a0a42d68badbeeef6024b9dbd81ef8a
Julio Nicolas Santarelli <santarellijulio@gmail.com>
2023-06-06 23:00:23 -0300

    prescripcionDigital(feat): agrega funciones de orden superior

--- prescripcion-digital/assets/js/funciones.js
+++ prescripcion-digital/assets/js/funciones.js
@@ -1,18 +1,55 @@
+meds = ''
 
-let stock = parseInt(prompt('Ingrese una cantidad de articulos (una cifra): '))
-
-// Función
-function recorrerMedicamentos() {
-  // Ciclo
-  for(let i = 0; i < stock; i++) {
-    console.log(stock, i)
-    }
-    // Condicional
-    if (stock >= 7) {
-      console.error('No tenemos esa cantidad de articulos')
-    } else {
-      console.warn('Estos son los articulos en stock: ', stock)
-      }
+mostrarMedicamentos = () => {
+  for(let i in vademecum) {
+    meds = `${vademecum[i].nombreComercial}, ${vademecum[i].presentacion.cantidad}`
+    console.log(meds)
   }
 
-recorrerMedicamentos()
+}
+
+mostrarMedicamentos()
+
+
+// Map + filter
+function listarCategorias() {
+  const arrayCategorias = vademecum.map ((med)=> { return med.presentacion.formaFarmaceutica})
+  const categoriasUnicas = [...new Set (arrayCategorias)]
+  console.table (categoriasUnicas)
+
+  categoriasUnicas.forEach((formaFarmaceutica)=> {
+      console.log(formaFarmaceutica)
+      const resultados = vademecum.filter((med)=> med.presentacion.formaFarmaceutica === formaFarmaceutica)
+      console.table(resultados)
+  })
+}
+
+listarCategorias()
+
+
+// Obtengo cantidad de medicación inicial
+let cantidadMedicacion = parseInt(prompt('Ingrese cantidad de comprimidos disponibles: '))
+console.log(cantidadMedicacion)
+
+// Obtengo duración del tratamiento
+let duracionTratamiento = parseInt(prompt('Ingrese duración del tratamiento (días): '))
+console.log(duracionTratamiento)
+
+// Obtengo duración del tratamiento
+let dosisDiaria = parseInt(prompt('Cuántos comprimidos diarios debe consumir? '))
+console.log(dosisDiaria)
+
+calcularDosis = () => {
+  let dosisTotal = dosisDiaria * duracionTratamiento
+  let diferenciaDosis = dosisTotal - cantidadMedicacion
+
+  if (dosisTotal <= cantidadMedicacion) {
+    alert('Tienes suficiente medicación: ' + dosisTotal + ' comprimidos')
+  } else {
+    alert('Necesitarás: ' + diferenciaDosis + ' comprimidos extra')
+    console.warn('Necesitarás: ' + diferenciaDosis + ' comprimidos extra')
+  }
+
+}
+
+calcularDosis()
